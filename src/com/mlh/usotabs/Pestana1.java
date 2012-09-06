4f12358e4c47f586a0b163f1511ce90c66339b28
mario <laserna.mario@gmail.com>
2012-09-05 21:17:33 -0500

    commit inicial

---
+++ src/com/mlh/usotabs/Pestana1.java
@@ -0,0 +1,26 @@
+package com.mlh.usotabs;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+
+public class Pestana1 extends Activity{
+	
+	private static final int latitudeE6 = 19240000;
+	private static final int longitudeE6 = -99120000;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pestana1);
+        
+        
+       
+	}
+	
+	
+	
+}
---
+++ src/com/mlh/usotabs/Pestana4.java
@@ -0,0 +1,12 @@
+package com.mlh.usotabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pestana4 extends Activity{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pestana4);
+    }
+}
